912b40404e3da49e84f65fa81a5675bb81404c52
William Njie <wsnjie@gmail.com>
2018-10-24 05:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-
+var band = []
+for (var i + 0; i < 4; i++){
+  band.push("${muscians[i]} plays ${instruments[i]}")}
+return band
 }
